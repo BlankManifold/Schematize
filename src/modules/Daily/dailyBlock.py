e731c940da89979fca26187781ab2d114025fb5e
BlankManifold <blankmanifold@gmail.com>
2022-11-26 18:11:40 +0100

    base dailymanager layout and logic

--- src/modules/Daily/dailyBlock.py
+++ src/modules/Daily/dailyBlock.py
@@ -1,10 +1,23 @@
+from datetime import timedelta
+
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty
+
 
 class DailyBlock(GridLayout):
-    
-    hour_range = ObjectProperty(None)
+
+    range_hour_text = StringProperty("")
     activities = ObjectProperty(None)
 
-    def __init__(self, **kwargs):
+    def __init__(self, range_hour: list[timedelta], **kwargs) -> None:
         super(DailyBlock, self).__init__(**kwargs)
+        self.range_hour = range_hour
+        self.update_range_text()
+
+    def update_range_text(self) -> None:
+        min_hour = self.range_hour[0].seconds // 3600
+        max_hour = self.range_hour[1].seconds // 3600
+        self.range_hour_text = f"{min_hour}:00-{max_hour}:00"
+
+    def clean(self) -> None:
+        self.activities.text = ""
--- src/modules/Daily/dailyManager.py
+++ src/modules/Daily/dailyManager.py
@@ -1,8 +1,54 @@
+from datetime import date, timedelta
+
 from kivy.uix.screenmanager import Screen
+from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty, StringProperty
+
 
 from modules.Daily.dailyBlock import DailyBlock
 kv = Builder.load_file("kvFiles/Daily.kv")
 
+
 class DailyManager(Screen):
-    pass
+
+    grid = ObjectProperty(None)
+    date_label = StringProperty(None)
+
+    def __init__(self, min_hour: int = 6, max_hour: int = 22, base_separation: int = 4, **kwargs) -> None:
+        super(DailyManager, self).__init__(**kwargs)
+
+        self.current_date = date.today()
+        self.date_label = str(self.current_date)
+        self.min_hour = timedelta(hours=min_hour)
+        self.max_hour = timedelta(hours=max_hour)
+        self.base_separation = timedelta(hours=base_separation)
+        self.num_blocks = int(
+            (self.max_hour - self.min_hour) / self.base_separation)
+        self.loaded = False
+        self.blocks = list[DailyBlock]()
+
+    def on_pre_enter(self) -> None:
+        if not self.loaded:
+            self.loaded = True
+            self.add_blocks()
+
+    def go_back(self) -> None:
+        self.manager.current = "Main"
+
+    def add_blocks(self) -> None:
+        for i in range(self.num_blocks):
+            begin = self.min_hour+i*self.base_separation
+            end = begin+self.base_separation
+            block = DailyBlock([begin, end])
+            self.grid.add_widget(block, index=1)
+            self.blocks.append(block)
+
+    def clear_blocks(self) -> None:
+        for block in self.blocks:
+            block.clean()
+
+    def change_day(self, next: bool) -> None:
+        self.current_date = self.current_date + timedelta(days=int(next)*2-1)
+        self.date_label = str(self.current_date)
+        self.clear_blocks()
